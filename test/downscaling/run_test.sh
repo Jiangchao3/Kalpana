238cbffe22c84c4fff540ccddec5cdab2c09cc96
William Pringle <wpringle@anl.gov>
2021-03-11 17:41:07 +0000

    modifying run_test to use ex 3 as the base location generator which worked for me with the grow examples (ex 1 did not)

--- test/downscaling/run_test.sh
+++ test/downscaling/run_test.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 ########################################################################################
-# This script runs the downscaling test examples detailed at the following website:    #
+# This script runs a modification of the downscaling test examples that are            #
+# detailed at the following website:                                                   #
 # https://ccht.ccee.ncsu.edu/downscaling-flooding-inundation-extents-using-kalpana/    #
+#                                                                                      #
+# Modifications: Changed to meter output instead of feet. Using Example 3 which uses   #
+# the base resolution and projection instead of the specified one which caused         #
+# caused problems for me with python3/grass78                                          #
 #                                                                                      #
 # Requires: - python3 with matplotlib, shapely, netCDF4, fiona, grass.script modules   #
 #           - grass78                                                                  #      
 #           - zip/unzip                                                                #
 #           - wget                                                                     #
 #                                                                                      #
-# By William Pringle, Mar 10 2021                                                      #
+# By William Pringle, March 2021                                                       #
 #    Argonne National Laboratory                                                       # 
 ########################################################################################
 python=python3
@@ -33,17 +38,17 @@ if [ ! -d "grdn35w078_13" ]; then
    rm temp.zip
 fi
 
-#Example 1: Specified resolution and EPSG code.
-echo "Running Example 1 for running the latter examples using r.grow"
-$python $kalpana --createlocation yes --raster "./grdn35w077_13/w001001.adf" --resolution 50 --epsg 2264 --vunitconv m2ft
+#Example 3: Native resolution and EPSG code
+echo "Running Example 3 for running the latter examples using r.grow"
+$python $kalpana --createlocation yes --raster "./grdn35w077_13/w001001.adf" --createmethod existing
 
-#Example 7: Keep original raster resolution and datum using multiple rasters.
+#Example 7: Grow using default inputs
 echo "Running Example 7"
-$python $kalpana --storm florence --filetype maxele.63.nc --contourrange "0 21 0.5" --units english --grow yes --grownoutput WaterLevels_grownDefaults --growmethod without --growradius 30.01 --flooddepth no --grownfiletype ESRI_Shapefile
+$python $kalpana --storm florence --filetype maxele.63.nc --contourrange "0 10 0.1" --grow yes --grownoutput WaterLevels_grownDefaults --growmethod without --growradius 30.01 --flooddepth no --grownfiletype ESRI_Shapefile
 
 #Example 10: Visualize grown results as flood depths.
 echo "Running Example 10"
-$python $kalpana --storm florence --filetype maxele.63.nc --contourrange "0 21 0.5" --units english --grow yes --grownoutput FloodDepth --flooddepth yes
+$python $kalpana --storm florence --filetype maxele.63.nc --contourrange "0 10 0.1" --units english --grow yes --grownoutput FloodDepth --flooddepth yes
 
 #Example 6: Keep original raster resolution and datum using multiple rasters.
 #echo "Running Example 6 to test multiple dems"
