c1f07da1a931e423ebb3276badd3e47c3c98d71e
William Pringle <wpringle@anl.gov>
2021-03-11 03:16:01 +0000

    updating test examples to run Ex 1 before Ex 7 and 10

--- test/downscaling/run_test.sh
+++ test/downscaling/run_test.sh
@@ -33,10 +33,18 @@ if [ ! -d "grdn35w078_13" ]; then
    rm temp.zip
 fi
 
-#Example 6: Keep original raster resolution and datum using multiple rasters.
-echo "Running Example 6"
-$python $kalpana --createlocation yes --raster "./grdn35w077_13/w001001.adf,./grdn35w078_13/w001001.adf" --createmethod existing --vunitconv m2f
+#Example 1: Specified resolution and EPSG code.
+echo "Running Example 1 for running the latter examples using r.grow"
+$python $kalpana --createlocation yes --raster "./grdn35w077_13/w001001.adf" --resolution 50 --epsg 2264 --vunitconv m2ft
 
 #Example 7: Keep original raster resolution and datum using multiple rasters.
 echo "Running Example 7"
 $python $kalpana --storm florence --filetype maxele.63.nc --contourrange "0 21 0.5" --units english --grow yes --grownoutput WaterLevels_grownDefaults --growmethod without --growradius 30.01 --flooddepth no --grownfiletype ESRI_Shapefile
+
+#Example 10: Visualize grown results as flood depths.
+echo "Running Example 10"
+$python $kalpana --storm florence --filetype maxele.63.nc --contourrange "0 21 0.5" --units english --grow yes --grownoutput FloodDepth --flooddepth yes
+
+#Example 6: Keep original raster resolution and datum using multiple rasters.
+#echo "Running Example 6 to test multiple dems"
+#$python $kalpana --createlocation yes --raster "./grdn35w077_13/w001001.adf,./grdn35w078_13/w001001.adf" --createmethod existing --vunitconv m2f
